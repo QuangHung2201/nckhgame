dc473b8b6f48f3e5e70fcc113c5e5043e5a68733
QuangHung <quanghung2201itt@gmail.com>
2026-03-23 08:22:50 +0700

    update

--- Assets/Scripts/Data/APImanager.cs
+++ Assets/Scripts/Data/APImanager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public static class APImanager 
 {
-    //public string callAPI(string textpost)
-    //{
-    //    APIchatBoxAI aPIchatBoxAI = new APIchatBoxAI();
-    //    aPIchatBoxAI.StartCoroutine(aPIchatBoxAI.PostReQuest(textpost)); // gọi api để lấy data
-    //    return aPIchatBoxAI.textResual;
-    //}
+    public static string textReSual;
+  //  public static string callAPI(string textpost)
+  //  {
+  //      APIchatBoxAI aPIchatBoxAI = new APIchatBoxAI();
+  //      
+  //      
+  //  }
 }
